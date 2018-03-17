70e21c97770aa23d9d904ecfc91e78cb20a15711
zlin3 <tslzf@hotmail.com>
2018-03-16 18:50:15 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {
+//     pageTitle: 'maintence page',
+//     messageContent: 'THe site is in maintenance mode.'
+//   });
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () =>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) =>{
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  //res.send('<h1>hello express!!</h1>');
+  // res.send({
+  //   name: 'Aaron',
+  //   likes: [
+  //     'sports',
+  //     'travelling'
+  //   ]
+  // });
+  res.render('home.hbs', {
+    pageTitle: 'home page',
+    messageContent: 'Welcome to my home page'
+  });
+});
+
+app.get('/about', (req, res) => {
+  //res.send('<h1>hello express!!</h1>');
+  res.render('about.hbs', {
+    pageTitle: 'About page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  //res.send('<h1>hello express!!</h1>');
+  res.send({
+    errorMessage: 'Unable to fulfill the request',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
